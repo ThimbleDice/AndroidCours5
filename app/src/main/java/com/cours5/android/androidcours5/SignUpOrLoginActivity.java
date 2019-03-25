4e68ddf22e9d15cf0f46db6fbc94c3172087826a
B533P-14\533 <ThimbleDice>
2019-03-25 10:28:17 -0400

    SignIn, SignUp, SignOut

---
+++ app/src/main/java/com/cours5/android/androidcours5/SignUpOrLoginActivity.java
@@ -0,0 +1,41 @@
+package com.cours5.android.androidcours5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpOrLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_or_login);
+        setListener();
+    }
+
+    private void setListener(){
+        findViewById(R.id.button_goToLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToLoginActivity();
+            }
+        });
+        findViewById(R.id.button_goToSignUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sentToSignUpActivity();
+            }
+        });
+    }
+
+    private void sentToSignUpActivity(){
+        Intent sendToSignUp = new Intent(this, SignUpActivity.class);
+        startActivity(sendToSignUp);
+    }
+
+    private void sendToLoginActivity(){
+        Intent sendToLogin = new Intent(this, LoginActivity.class);
+        startActivity(sendToLogin);
+    }
+}
